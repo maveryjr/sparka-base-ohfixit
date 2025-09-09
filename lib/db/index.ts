21aa635411358a1195b905f08825aec3bc3913d5
Michael Avery <mla2090@email.vccs.edu>
2025-09-09 19:52:22 -0400

    refactor: remove Prisma and auth dependencies in favor of Drizzle

--- lib/db/index.ts
+++
@@ -1,8 +0,0 @@
-// Export the Prisma client and any database utilities
-export * from './client';
-// Export any other database-related utilities or types
-export * from './migrate';
-
-// If you have any database utility functions, export them here
-// Example:
-// export { someDbUtility } from './utils';
